5bc344936c6d466d1caaafcf68bdcb644df2ddbc
zeinanada <34456457+zeinanada@users.noreply.github.com>
2018-09-09 14:32:19 +0200

    Update script.sh

--- django_app/script.sh
+++ django_app/script.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-#kill $(ps aux | grep '[m]otion' | awk '{print $2}')
-
-#ps -ef | grep  motion | grep -v grep | awk '{print $2}' | xargs kill
-
-#if pgrep motion; then pkill motion; fi
-
 
 if pgrep motion
 then
 echo "motion running "
-#kill -9 $(ps | grep "motion" | grep -v grep | awk '{ print $1 }')
 killall motion
 echo "motion  got killed"
 else
